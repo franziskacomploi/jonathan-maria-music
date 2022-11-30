08efff9d3e43b3a6ad3c049f2f43bd9fd9117d9d
Franziska Pendzialek <franziska.pendzialek@outlook.com>
2022-11-30 10:25:30 +0100

    clean up package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/styles/tailwind.css'],
-  buildModules: ['@nuxt/typescript-build', '@nuxt/postcss8'],
+  buildModules: ['@nuxt/typescript-build'],
   postcss: {
     plugins: {
       tailwindcss: {},
